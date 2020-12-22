42d4bbf0ca3da85f47f1a9de61176818cd682975
Matthew Coburn <mattcoburn24@gmail.com>
2020-12-22 01:32:28 -0800

    bug check

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -4,25 +4,25 @@ import TaskList from "./TaskList";
 import * as TaskStories from './Task.stories'
 
 export default {
+    component: TaskList,
     title: 'TaskList',
-    component: Tasklist,
     decorators: [story => <div style={{ padding: '3rem'}}>{story()}</div>]
 }
 
 const Template = (args) => <TaskList {...args} />;
 
-export const Default = Template.bind({})
+export const Default = Template.bind({});
 Default.args = {
     tasks: [
-        {...TaskStories.default.args.task, id: '1', title: 'task 1' }
-        {...TaskStories.default.args.task, id: '2', title: 'task 2' }
-        {...TaskStories.default.args.task, id: '3', title: 'task 3' }
-        {...TaskStories.default.args.task, id: '4', title: 'task 4' }
-        {...TaskStories.default.args.task, id: '5', title: 'task 5' }
+        {...TaskStories.Default.args.task, id: '1', title: 'task 1' }
+        {...TaskStories.Default.args.task, id: '2', title: 'task 2' }
+        {...TaskStories.Default.args.task, id: '3', title: 'task 3' }
+        {...TaskStories.Default.args.task, id: '4', title: 'task 4' }
+        {...TaskStories.Default.args.task, id: '5', title: 'task 5' }
     ]
 }
 
-export const withPinnedTask = Template.bind({})
+export const withPinnedTask = Template.bind({});
 withPinnedTask.args = {
     tasks: [
     ...Default.args.tasks.slice(0,5), 
@@ -30,13 +30,13 @@ withPinnedTask.args = {
     ]
 }
 
-export const Loading = Template.bind({})
+export const Loading = Template.bind({});
 Loading.args = {
     tasks: [],
     loading: true
 }
 
-export const Empty = Template.bind({})
+export const Empty = Template.bind({});
 Empty.args = {
     ...Loading.args,
     loading: false
