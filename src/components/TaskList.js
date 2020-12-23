d4f49f50543d1384fd668813a4ec6ecb19aab6ad
Matthew Coburn <mattcoburn24@gmail.com>
2020-12-22 21:45:50 -0800

    finished TaskList story and set up a unit test for it

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,27 +1,65 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import Task from "./Task";
 
-export default TaskList({ loading, tasks, onPinTask, onArchiveTask }) => {
+export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
   };
 
+  const loadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
   if (loading) {
-    return <div className="list-items">Loading</div>;
+    return (
+      <div className="list-items">
+        {loadingRow}
+        {loadingRow}
+        {loadingRow}
+        {loadingRow}
+        {loadingRow}
+        {loadingRow}
+      </div>
+    );
   }
 
   if (tasks.length === 0) {
-    return <div className="list-items">Empty</div>;
+    return (
+      <div className="list-items">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <div className="title-message">You have no tasks</div>
+          <div className="subtitle-message">Sit back and relax</div>
+        </div>
+      </div>
+    );
   }
 
+  const tasksInOrder = [
+    ...tasks.filter((task) => task.state === "TASK_PINNED"),
+    ...tasks.filter((task) => task.state !== "TASK_PINNED"),
+  ];
+
   return (
-    <div className="task-list">
-      {tasks.map((task) => (
+    <div className="list-items">
+      {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
   );
+}
+
+TaskList.propTypes = {
+  loading: PropTypes.bool,
+  tasks: PropTypes.arrayOf(Task.propTypes.task),
+  onPinTask: PropTypes.func,
+  onArchiveTask: PropTypes.func,
 };
-
-
